4dc426b05228c62209f87fbacf9af61c506e4c5a
xiangjingbo <xiangjingbo@mchz.com.cn>
2019-03-10 21:45:32 +0800

    add 1 16

---
+++ 001 Two Sum.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Author  : mofei
+# @Time    : 2018/10/26 19:54
+# @File    : 001 Two Sum.py
+# @Software: PyCharm
+
+"""
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+"""
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """和为target的两个数字的下标"""
+        dict = {}
+        for index, num in enumerate(nums):
+            if target - num in dict:
+                return [dict[target - num], index]
+            dict[num] = index
+
+
+if __name__ == '__main__':
+    result = Solution().twoSum([2, 7, 11, 15], 9)
+    print(result)
---
+++ 016 3Sum Closest.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Author  : mofei
+# @Time    : 2019/3/10 20:19
+# @File    : 016 3Sum Closest.py
+# @Software: PyCharm
+
+"""
+Given an array nums of n integers and an integer target, find three integers in nums such that the sum is closest to target.
+Return the sum of the three integers. You may assume that each input would have exactly one solution.
+
+Example:
+Given array nums = [-1, 2, 1, -4], and target = 1.
+The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+"""
+from typing import List
+
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        if len(nums) < 3:
+            return 0
+        if len(nums) == 3:
+            return sum(nums)
+        nums.sort()
+        closest = sum(nums[:3])
+        for i in range(len(nums) - 2):
+            j, k = i + 1, len(nums) - 1
+            while j < k:
+                cur_sum = nums[i] + nums[j] + nums[k]
+                if cur_sum == target:
+                    return cur_sum
+                if abs(cur_sum - target) < abs(closest - target):
+                    closest = cur_sum
+                if cur_sum > target:
+                    k -= 1
+                else:
+                    j += 1
+        return closest
+
+    def threeSumClosest2(self, nums: List[int], target: int) -> int:
+        def closest_func(cur_sum):
+            if cur_sum == target:
+                return cur_sum
+            if abs(cur_sum - target) < abs(closest - target):
+                return cur_sum
+            return closest
+        if len(nums) < 3:
+            return 0
+        if len(nums) == 3:
+            return sum(nums)
+        nums.sort()
+        closest = sum(nums[:3])
+        for i in range(len(nums) - 2):
+            j, k = i + 1, len(nums) - 1
+            if nums[i] + nums[k - 1] + nums[k] <= target:
+                closest = closest_func(nums[i] + nums[k - 1] + nums[k])
+            elif nums[i] + nums[j] + nums[j + 1] >= target:
+                closest = closest_func(nums[i] + nums[j] + nums[j + 1])
+            else:
+                while j < k:
+                    cur_sum = nums[i] + nums[j] + nums[k]
+                    closest = closest_func(cur_sum)
+                    if cur_sum > target:
+                        k -= 1
+                    else:
+                        j += 1
+            if closest == target:
+                return target
+        return closest
+
+
+if __name__ == '__main__':
+    result = Solution().threeSumClosest2([1, 2, 4, 8, 16, 32, 64, 128], 82)
+    print(result)
