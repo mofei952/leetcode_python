ea46c117d1b7d658e03798b3164ee9c8e3a8e46c
mofei952 <695741200@qq.com>
2019-04-17 20:27:47 +0800

    add 104 771

---
+++ 104 Maximum Depth of Binary Tree.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Author  : mofei
+# @Time    : 2019/4/17 19:11
+# @File    : 104 Maximum Depth of Binary Tree.py
+# @Software: PyCharm
+
+"""
+Given a binary tree, find its maximum depth.
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Note: A leaf is a node with no children.
+
+Example:
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its depth = 3.
+"""
+import random
+
+import pytest
+
+from commons import func_test
+from tree.tree_node import TreeNode
+
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        """数的最大深度"""
+        if not root:
+            return 0
+        stack = [root]
+        depth = 0
+        while stack:
+            depth += 1
+            t = []
+            for node in stack:
+                if node.left:
+                    t.append(node.left)
+                if node.right:
+                    t.append(node.right)
+            stack = t
+        return depth
+
+    def maxDepth2(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        return max(self.maxDepth2(root.left), self.maxDepth2(root.right)) + 1
+
+
+@pytest.fixture(scope="module")
+def test_data():
+    params_list = [
+        (TreeNode.create_tree([3, 9, 20, None, None, 15, 7]),),
+    ]
+    res_list = [
+        3,
+    ]
+    for i in range(0, 2999):
+        v = TreeNode.create_tree([random.randint(0, i * 10) for i in range(i)])
+        params_list.append((v,))
+        res_list.append(Solution().maxDepth(v))
+    return params_list, res_list, 1
+
+
+def test_007(test_data):
+    func_test(Solution().maxDepth, *test_data)
+
+
+def test_007_2(test_data):
+    func_test(Solution().maxDepth2, *test_data)
+
+
+if __name__ == '__main__':
+    pytest.main(['-vv', '--durations=10', '-q', '--tb=line', '-x', '104 Maximum Depth of Binary Tree.py'])
+    # pytest.main(['-x', '009 Palindrome Number.py'])
---
+++ hash_table/771 Jewels and Stones.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Author  : mofei
+# @Time    : 2019/4/17 19:55
+# @File    : 771 Jewels and Stones.py
+# @Software: PyCharm
+
+"""
+You're given strings J representing the types of stones that are jewels, and S representing the stones you have.
+Each character in S is a type of stone you have.
+You want to know how many of the stones you have are also jewels.
+
+The letters in J are guaranteed distinct, and all characters in J and S are letters.
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+Example 1:
+Input: J = "aA", S = "aAAbbbb"
+Output: 3
+
+Example 2:
+Input: J = "z", S = "ZZ"
+Output: 0
+
+Note:
+S and J will consist of letters and have length at most 50.
+The characters in J are distinct.
+"""
+
+
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        set = {v for v in J}
+        count = 0
+        for v in S:
+            if v in set:
+                count += 1
+        return count
+
+
+if __name__ == '__main__':
+    print(Solution().numJewelsInStones('aA', 'aAAbbbb'))
+    print(Solution().numJewelsInStones('z', 'ZZ'))
