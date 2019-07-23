fd55a08ee2eacf6c811160fb02eaab9cfbb1c5ab
mofei952 <695741200@qq.com>
2019-07-23 20:29:55 +0800

    update 16

--- other/016 3Sum Closest.py
+++ other/016 3Sum Closest.py
@@ -14,14 +14,12 @@ Example:
 Given array nums = [-1, 2, 1, -4], and target = 1.
 The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
 """
+
 from typing import List
 
 
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        """nums中和最接近target的三个数的和"""
-        if len(nums) < 3:
-            return 0
         if len(nums) == 3:
             return sum(nums)
         nums.sort()
@@ -29,50 +27,55 @@ class Solution:
         for i in range(len(nums) - 2):
             j, k = i + 1, len(nums) - 1
             while j < k:
-                cur_sum = nums[i] + nums[j] + nums[k]
-                if cur_sum == target:
-                    return cur_sum
-                if abs(cur_sum - target) < abs(closest - target):
-                    closest = cur_sum
-                if cur_sum > target:
-                    k -= 1
-                else:
+                current = nums[i] + nums[j] + nums[k]
+                if current == target:
+                    return target
+                if abs(current - target) < abs(closest - target):
+                    closest = current
+                if current < target:
                     j += 1
+                else:
+                    k -= 1
         return closest
 
     def threeSumClosest2(self, nums: List[int], target: int) -> int:
-        """nums中和最接近target的三个数的和"""
-        def closest_func(cur_sum):
-            if cur_sum == target:
-                return cur_sum
-            if abs(cur_sum - target) < abs(closest - target):
-                return cur_sum
-            return closest
-        if len(nums) < 3:
-            return 0
         if len(nums) == 3:
             return sum(nums)
         nums.sort()
         closest = sum(nums[:3])
         for i in range(len(nums) - 2):
-            j, k = i + 1, len(nums) - 1
-            if nums[i] + nums[k - 1] + nums[k] <= target:
-                closest = closest_func(nums[i] + nums[k - 1] + nums[k])
-            elif nums[i] + nums[j] + nums[j + 1] >= target:
-                closest = closest_func(nums[i] + nums[j] + nums[j + 1])
-            else:
-                while j < k:
-                    cur_sum = nums[i] + nums[j] + nums[k]
-                    closest = closest_func(cur_sum)
-                    if cur_sum > target:
-                        k -= 1
-                    else:
-                        j += 1
-            if closest == target:
+            if i and nums[i] == nums[i - 1]:
+                continue
+            min_sum = nums[i] + nums[i + 1] + nums[i + 2]
+            max_sum = nums[i] + nums[-2] + nums[-1]
+            if min_sum == target or max_sum == target:
                 return target
+            if min_sum > target:
+                if abs(min_sum - target) < abs(closest - target):
+                    closest = min_sum
+                return closest
+            if max_sum < target:
+                if abs(max_sum - target) < abs(closest - target):
+                    closest = max_sum
+                continue
+            j, k = i + 1, len(nums) - 1
+            while j < k:
+                current = nums[i] + nums[j] + nums[k]
+                if current == target:
+                    return target
+                if abs(current - target) < abs(closest - target):
+                    closest = current
+                if current < target:
+                    j += 1
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
+                else:
+                    k -= 1
+                    while j < k and nums[k] == nums[k + 1]:
+                        k -= 1
         return closest
 
 
 if __name__ == '__main__':
-    result = Solution().threeSumClosest2([1, 2, 4, 8, 16, 32, 64, 128], 82)
-    print(result)
+    print(Solution().threeSumClosest2([1, 1, 1, 1], 100))
+    print(Solution().threeSumClosest2([1, 2, 4, 8, 16, 32, 64, 128], 82))
