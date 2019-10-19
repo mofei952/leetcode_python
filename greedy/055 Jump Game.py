fb35fd064e68ba67334e13f722d8be494e78fcf5
mofei952 <695741200@qq.com>
2019-10-19 21:54:00 +0800

    update 55 jump game

--- greedy/055 Jump Game.py
+++ greedy/055 Jump Game.py
@@ -25,8 +25,11 @@ Explanation: You will always arrive at index 3 no matter what. Its maximum
 
 from typing import List
 
+from commons import running_time
+
 
 class Solution:
+    @running_time
     def canJump(self, nums: List[int]) -> bool:
         max_distance = 0
         for i in range(len(nums)):
@@ -35,8 +38,21 @@ class Solution:
             max_distance = max(max_distance, i + nums[i])
         return True
 
+    @running_time
+    def canJump2(self, nums: List[int]) -> bool:
+        last_index = len(nums) - 1
+        for i in range(last_index - 1, -1, -1):
+            if i + nums[i] >= last_index:
+                last_index = i
+        return last_index == 0
+
 
 if __name__ == '__main__':
-    print(Solution().canJump([2, 3, 1, 1, 4]))
-    print(Solution().canJump([3, 2, 1, 0, 4]))
-    print(Solution().canJump([2, 0, 0]))
+    assert Solution().canJump([2, 3, 1, 1, 4]) is True
+    assert Solution().canJump2([2, 3, 1, 1, 4]) is True
+
+    assert Solution().canJump([3, 2, 1, 0, 4]) is False
+    assert Solution().canJump2([3, 2, 1, 0, 4]) is False
+
+    assert Solution().canJump([2, 0, 0]) is True
+    assert Solution().canJump2([2, 0, 0]) is True
