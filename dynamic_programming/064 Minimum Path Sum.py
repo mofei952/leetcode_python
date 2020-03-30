7f179a4e70f1f42b2a4f6c0bc88955c68789336a
mofei952 <695741200@qq.com>
2020-03-30 21:19:16 +0800

    update 64 Minimum Path Sum

--- dynamic_programming/064 Minimum Path Sum.py
+++ dynamic_programming/064 Minimum Path Sum.py
@@ -26,42 +26,27 @@ from typing import List
 
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        if not grid:
-            return 0
-        row = len(grid)
-        col = len(grid[0])
-        dp = [[0 for j in range(col)] for i in range(row)]
-        for i in range(row):
-            for j in range(col):
-                dp[i][j] = grid[i][j]
-                if i and not j:
-                    dp[i][j] += dp[i - 1][j]
-                elif not i and j:
-                    dp[i][j] += dp[i][j - 1]
-                elif i and j:
-                    dp[i][j] += min(dp[i - 1][j], dp[i][j - 1])
-        return dp[row - 1][col - 1]
+        m = len(grid)
+        n = len(grid[0])
 
-    def minPathSum2(self, grid: List[List[int]]) -> int:
-        if not grid:
-            return 0
-        row = len(grid)
-        col = len(grid[0])
-        dp = [0] * col
+        dp = [0] * n
+
         dp[0] = grid[0][0]
-        for i in range(1, len(dp)):
+        for i in range(1, n):
             dp[i] = dp[i - 1] + grid[0][i]
-        for i in range(1, row):
-            dp[0] += grid[i][0]
-            for j in range(1, col):
+
+        for i in range(1, m):
+            dp[0] = dp[0] + grid[i][0]
+            for j in range(1, n):
                 dp[j] = min(dp[j - 1], dp[j]) + grid[i][j]
+
         return dp[-1]
 
 
 if __name__ == '__main__':
-    print(Solution().minPathSum2([
+    print(Solution().minPathSum([
         [1, 3, 1],
         [1, 5, 1],
         [4, 2, 1]
     ]))
-    print(Solution().minPathSum2([[1]]))
+    print(Solution().minPathSum([[1]]))
