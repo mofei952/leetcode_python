d70f021d227dba57ef518a04275db53ca33e345f
mofei952 <695741200@qq.com>
2020-08-17 21:56:29 +0800

    update 126 Word Ladder II

--- backtracking/126 Word Ladder II.py
+++ backtracking/126 Word Ladder II.py
@@ -33,51 +33,49 @@ Explanation: The endWord "cog" is not in wordList, therefore no possible transfo
 """
 
 from typing import List
+from collections import defaultdict
 
 
 class Solution:
     def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
-        shortest_length = len(wordList)
+        words = set(wordList)
+        if endWord not in wordList:
+            return []
+        
+        graph = defaultdict(set)
         word_length = len(wordList[0])
-        result = []
+        bq = {beginWord}
+        eq = {endWord}
+        end = False
+        rev = False
 
-        cur_word = beginWord
-        sequence = [cur_word]
-        visited_set = {cur_word}
+        while bq and not end:
+            temp = set()
+            words -= bq
+            for x in bq:
+                for i in range(word_length):
+                    first, second = x[:i], x[i+1:]
+                    for c in 'qwertyuiopasdfghjklzxcvbnm':
+                        y = first + c + second
+                        if y in words:
+                            temp.add(y)
+                            graph[y].add(x) if rev else graph[x].add(y)
+                            if y in eq:
+                                end = True
+            bq = temp
+            if len(bq) > len(eq):
+                bq, eq, rev = eq, bq, not rev
 
-        def dfs():
-            nonlocal shortest_length, cur_word
-            cur_word = sequence[-1]
+        def bt(x):
+            if x == endWord:
+                return [[x]]
+            res = []
+            for y in graph[x]:
+                for rest in bt(y):
+                    res.append([x] + rest)
+            return res
 
-            # print('seq:', sequence)
-
-            if cur_word == endWord:
-                result.append(list(sequence))
-                if len(sequence) < shortest_length:
-                    shortest_length = len(sequence)
-                return
-            if len(sequence) >= shortest_length:
-                return
-
-            words = [
-                w for w in wordList
-                if w not in visited_set and sum(
-                    [cur_word[i] != w[i] for i in range(word_length)]
-                ) == 1
-            ]
-
-            # print('word:', words)
-
-            for w in words:
-                sequence.append(w)
-                visited_set.add(w)
-                dfs()
-                sequence.pop()
-                visited_set.remove(w)
-
-        dfs()
-        result = [seq for seq in result if len(seq) == shortest_length]
-        return result
+        return bt(beginWord)
 
 
 if __name__ == "__main__":
@@ -85,3 +83,5 @@ if __name__ == "__main__":
                                  ["hot", "dot", "dog", "lot", "log", "cog"]))
     print(Solution().findLadders("hit", "cog",
                                  ["hot", "dot", "dog", "lot", "log"]))
+    print(Solution().findLadders("qa", "sq",
+                                 ["si", "go", "se", "cm", "so", "ph", "mt", "db", "mb", "sb", "kr", "ln", "tm", "le", "av", "sm", "ar", "ci", "ca", "br", "ti", "ba", "to", "ra", "fa", "yo", "ow", "sn", "ya", "cr", "po", "fe", "ho", "ma", "re", "or", "rn", "au", "ur", "rh", "sr", "tc", "lt", "lo", "as", "fr", "nb", "yb", "if", "pb", "ge", "th", "pm", "rb", "sh", "co", "ga", "li", "ha", "hz", "no", "bi", "di", "hi", "qa", "pi", "os", "uh", "wm", "an", "me", "mo", "na", "la", "st", "er", "sc", "ne", "mn", "mi", "am", "ex", "pt", "io", "be", "fm", "ta", "tb", "ni", "mr", "pa", "he", "lr", "sq", "ye"]))
