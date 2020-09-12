857d594532845eb7e6c7f08ad2726a77cff8a36a
mofei952 <695741200@qq.com>
2020-09-12 21:25:51 +0800

    update 154 Find Minimum in Rotated Sorted Array II

--- binary_search/154 Find Minimum in Rotated Sorted Array II.py
+++ binary_search/154 Find Minimum in Rotated Sorted Array II.py
@@ -36,10 +36,22 @@ class Solution:
                 low = mid + 1
         return nums[low]
 
+    def findMin2(self, nums: List[int]) -> int:
+        low, high = 0, len(nums) - 1
+        while low < high:
+            mid = (low + high) // 2
+            if nums[mid] == nums[high]:
+                high -= 1
+            elif nums[mid] < nums[high]:
+                high = mid
+            else:
+                low = mid + 1
+        return nums[low]
+
 
 if __name__ == "__main__":
-    print(Solution().findMin([1, 3, 5]))
-    print(Solution().findMin([2, 2, 2, 0, 1]))
-    print(Solution().findMin([3, 3, 1, 3]))
-    print(Solution().findMin([3, 1, 3, 3, 3]))
-    print(Solution().findMin([1, 1, 1]))
+    print(Solution().findMin2([1, 3, 5]))
+    print(Solution().findMin2([2, 2, 2, 0, 1]))
+    print(Solution().findMin2([3, 3, 1, 3]))
+    print(Solution().findMin2([3, 1, 3, 3, 3]))
+    print(Solution().findMin2([1, 1, 1]))
