3126b5542327f84d9e307242bd5ec86c68fc7ed9
mofei952 <695741200@qq.com>
2020-09-21 22:25:58 +0800

    update 76 Minimum Window Substring

--- two_pointers/076 Minimum Window Substring.py
+++ two_pointers/076 Minimum Window Substring.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import Counter, defaultdict
 
 """
 Given a string S and a string T, find the minimum window in S which will contain all the characters in T in complexity O(n).
@@ -18,7 +18,7 @@ class Solution:
         t_map = defaultdict(int)
         for x in t:
             t_map[x] += 1
-        
+
         s_map = defaultdict(int)
         i, j = 0, -1
         min_length, min_window = float('inf'), ''
@@ -37,9 +37,26 @@ class Solution:
                 if s[j] in t_map:
                     s_map[s[j]] += 1
         return min_window
-        
 
- 
+    def minWindow2(self, s: str, t: str) -> str:
+        need = defaultdict(int)
+        for x in t:
+            need[x] += 1
+        missing = len(t)
+        i, left, right = 0, 0, len(s)
+        for j in range(len(s)):
+            missing -= need[s[j]] > 0
+            need[s[j]] -= 1
+            if missing == 0:
+                while need[s[i]] < 0:
+                    need[s[i]] += 1
+                    i += 1
+                if j - i < right - left:
+                    left, right = i, j
+        return '' if right == len(s) else s[left: right+1]
+
+
 if __name__ == "__main__":
-    print(Solution().minWindow("ADOBECODEBANC", "ABC"))
-    print(Solution().minWindow("aa", "aa"))
+    print(Solution().minWindow2("ADOBECODEBANC", "ABC"))
+    print(Solution().minWindow2("aa", "aa"))
+    print(Solution().minWindow2("a", "aa"))
