16d01a033fa0a1b3c7cd4ff98e93323c46283fe3
mofei952 <695741200@qq.com>
2020-10-27 20:58:03 +0800

    add: 198 Hourse Robber

---
+++ dynamic_programming/198 House Robber.py
@@ -0,0 +1,48 @@
+"""
+You are a professional robber planning to rob houses along a street.
+Each house has a certain amount of money stashed, 
+the only constraint stopping you from robbing each of them is that 
+adjacent houses have security system connected and it will automatically 
+contact the police if two adjacent houses were broken into on the same night.
+
+Given a list of non-negative integers representing the amount of money of each house, 
+determine the maximum amount of money you can rob tonight without alerting the police.
+
+Example 1:
+Input: nums = [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+             Total amount you can rob = 1 + 3 = 4.
+
+Example 2:
+Input: nums = [2,7,9,3,1]
+Output: 12
+Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+             Total amount you can rob = 2 + 9 + 1 = 12.
+"""
+
+from typing import List
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        if len(nums) <= 2:
+            return max(nums)
+
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i-1], dp[i-2]+nums[i])
+
+        return dp[-1]
+
+
+if __name__ == "__main__":
+    print(Solution().rob([1, 2, 3, 1]))
+    print(Solution().rob([2, 7, 9, 3, 1]))
+    print(Solution().rob([]))
+    print(Solution().rob([0]))
