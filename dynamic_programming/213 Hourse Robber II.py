0e5c8a234452a60a50fdf4462c8b01f3b51e7c97
mofei952 <695741200@qq.com>
2020-11-13 21:36:42 +0800

    update: 213 Hourse Robber II

--- dynamic_programming/213 Hourse Robber II.py
+++ dynamic_programming/213 Hourse Robber II.py
@@ -32,8 +32,6 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         if not nums:
             return 0
-        if len(nums) == 1:
-            return nums[0]
 
         def _rob(nums):
             prev = curr = 0
