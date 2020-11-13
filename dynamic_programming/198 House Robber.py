67efc4985d98e55a0d6f7a274891b3eea4b4d7b2
mofei952 <695741200@qq.com>
2020-11-13 21:31:43 +0800

    update: 198 Hourse Robber

--- dynamic_programming/198 House Robber.py
+++ dynamic_programming/198 House Robber.py
@@ -26,19 +26,10 @@ from typing import List
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-        if len(nums) <= 2:
-            return max(nums)
-
-        dp = [0] * len(nums)
-        dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
-
-        for i in range(2, len(nums)):
-            dp[i] = max(dp[i-1], dp[i-2]+nums[i])
-
-        return dp[-1]
+        prev = curr = 0
+        for num in nums:
+            prev, curr = curr, max(prev+num, curr)
+        return curr
 
 
 if __name__ == "__main__":
