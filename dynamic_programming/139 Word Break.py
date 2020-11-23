9d9843d6d767c506df523eb42b5970f2cf866c74
mofei952 <695741200@qq.com>
2020-11-23 21:20:15 +0800

    update: 139 Word Break

--- dynamic_programming/139 Word Break.py
+++ dynamic_programming/139 Word Break.py
@@ -27,25 +27,34 @@ from typing import List
 
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        slen = len(s)
+        dp = [False] * (slen+1)
+        dp[0] = True
+        for i in range(1, slen+1):
+            for w in wordDict:
+                wlen = len(w)
+                if i >= wlen and dp[i-wlen] and s[i-wlen:i] == w:
+                    dp[i] = True
+                    break
+        return dp[-1]
+
+    def wordBreak2(self, s: str, wordDict: List[str]) -> bool:
+        if not wordDict:
+            return False
+        words = set(wordDict)
+        max_length = max(len(w) for w in words)
         dp = [True]
         for i in range(1, len(s)+1):
-            for j in range(len(wordDict)):
-                w = wordDict[j]
-                wlen = len(w)
-                if i >= wlen and s[i-wlen:i] == w and dp[i-wlen]:
-                    dp.append(True)
-                    break
-            else:
-                dp.append(False)
-        return dp[-1] 
+            dp.append(any(dp[j] and s[j:i] in words for j in range(max(i-max_length,0), i)))
+        return dp[-1]
+                    
 
 
 if __name__ == "__main__":
     print(Solution().wordBreak('leetcode', ["leet", "code"]))
     print(Solution().wordBreak('applepenapple', ["apple", "pen"]))
-    print(Solution().wordBreak('catsandog', [
-          "cats", "dog", "sand", "and", "cat"]))
+    print(Solution().wordBreak('catsandog', ["cats", "dog", "sand", "and", "cat"]))
     print(Solution().wordBreak('a', []))
     print(Solution().wordBreak("dogs", ["dog", "s", "gs"]))
     print(Solution().wordBreak("ccaccc", ["cc", "ac"]))
-    print(Solution().wordBreak("acccbccb", ["cc","bc","ac","ca"]))
+    print(Solution().wordBreak("acccbccb", ["cc", "bc", "ac", "ca"]))
