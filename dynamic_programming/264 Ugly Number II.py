ca7b856fc19e1ef7a8d8a2f2ecb0be0e54c4a9db
mofei952 <695741200@qq.com>
2020-11-26 20:01:46 +0800

    update: 264 Ugly Number II add cache

--- dynamic_programming/264 Ugly Number II.py
+++ dynamic_programming/264 Ugly Number II.py
@@ -17,25 +17,36 @@ n does not exceed 1690.
 
 
 class Solution:
+    def __init__(self):
+        self.dp = None
+
     def nthUglyNumber(self, n: int) -> int:
-        dp = [1]
+        if self.dp:
+            return self.dp[n-1]
+
+        self.dp = dp = []
+        heap = [1]
         num_set = set()
-        for _ in range(n):
-            x = heapq.heappop(dp)
+
+        for _ in range(1690):
+            x = heapq.heappop(heap)
+            dp.append(x)
             if 2*x not in num_set:
-                heapq.heappush(dp, 2*x)
+                heapq.heappush(heap, 2*x)
                 num_set.add(2*x)
             if 3*x not in num_set:
-                heapq.heappush(dp, 3*x)
+                heapq.heappush(heap, 3*x)
                 num_set.add(3*x)
-            heapq.heappush(dp, 5*x)
+            heapq.heappush(heap, 5*x)
             num_set.add(5*x)
-        return x
+
+        return dp[n-1]
 
     def nthUglyNumber2(self, n: int) -> int:
         dp = [1]
         i2 = i3 = i5 = 0
-        for _ in range(n-1):
+        
+        for _ in range(n):
             x = min(dp[i2]*2, dp[i3]*3, dp[i5]*5)
             dp.append(x)
             if x == dp[i2]*2:
@@ -44,8 +55,27 @@ class Solution:
                 i3 += 1
             if x == dp[i5]*5:
                 i5 += 1
+                
         return dp[-1]
-            
+    
+    def nthUglyNumber3(self, n: int) -> int:
+        if self.dp:
+            return self.dp[n-1]
+        
+        dp = [1]
+        i2 = i3 = i5 = 0
+        
+        for _ in range(1689):
+            x = min(dp[i2]*2, dp[i3]*3, dp[i5]*5)
+            dp.append(x)
+            if x == dp[i2]*2:
+                i2 += 1
+            if x == dp[i3]*3:
+                i3 += 1
+            if x == dp[i5]*5:
+                i5 += 1
+                
+        return dp[n-1]
 
 
 if __name__ == "__main__":
@@ -53,6 +83,6 @@ if __name__ == "__main__":
     t = time.time()
     s = Solution()
     for i in range(1, 1691):
-        Solution().nthUglyNumber2(i)
-        # print(i, s.nthUglyNumber(i))
+        Solution().nthUglyNumber(i)
+        # print(i, s.nthUglyNumber2(i))
     print(time.time() - t)
