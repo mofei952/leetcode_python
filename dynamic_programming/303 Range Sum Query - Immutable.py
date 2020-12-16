e206b0d310a5480e0f2377fd6dea8602ca5ea1a5
mofei952 <695741200@qq.com>
2020-12-16 21:51:55 +0800

    update: 303 Range Sum Query

--- dynamic_programming/303 Range Sum Query - Immutable.py
+++ dynamic_programming/303 Range Sum Query - Immutable.py
@@ -32,6 +32,17 @@ from typing import List
 class NumArray:
 
     def __init__(self, nums: List[int]):
+        self.dp = [0] + nums
+        for i in range(1, len(self.dp)):
+            self.dp[i] += self.dp[i-1]
+
+    def sumRange(self, i: int, j: int) -> int:
+        return self.dp[j+1] - self.dp[i]
+
+
+class NumArray2:
+
+    def __init__(self, nums: List[int]):
         self.dp = [0] * (len(nums)+1)
         for i in range(len(nums)):
             self.dp[i+1] = self.dp[i] + nums[i]
