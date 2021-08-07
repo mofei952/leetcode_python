9d763203275f0d80e87ed4ff2594a9080a36b1b7
mofei952 <695741200@qq.com>
2021-08-07 22:42:11 +0800

    perf: optimize solution for question 11

--- two_pointers/011 Container With Most Water.py
+++ two_pointers/011 Container With Most Water.py
@@ -1,24 +1,9 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# @Author  : mofei
-# @Time    : 2019/7/16 19:07
-# @File    : 011 Container With Most Water.py
-# @Software: PyCharm
-
 """
-Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai).
-n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0).
-Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+quesion:
+https://leetcode.com/problems/container-with-most-water/
 
-Note: You may not slant the container and n is at least 2.
-
-The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7].
-In this case, the max area of water (blue section) the container can contain is 49.
-
-Example:
-Input: [1,8,6,2,5,4,8,3,7]
-Output: 49
+solution:
+https://mofei952.github.io/2021/08/07/leetcode%2011%20Container%20With%20Most%20Water/
 """
 
 from typing import List
@@ -26,18 +11,36 @@ from typing import List
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        low, high = 0, len(height) - 1
+        i, j = 0, len(height) - 1
         max_area = 0
-        while low < high:
-            if height[low] < height[high]:
-                area = height[low] * (high - low)
-                low += 1
+        while i < j:
+            if height[i] < height[j]:
+                area = height[i] * (j - i)
+                i += 1
             else:
-                area = height[high] * (high - low)
-                high -= 1
+                area = height[j] * (j - i)
+                j -= 1
+            max_area = max(max_area, area)
+        return max_area
+
+    def maxArea2(self, height: List[int]) -> int:
+        i, j = 0, len(height) - 1
+        max_area = 0
+        while i < j:
+            if height[i] < height[j]:
+                area = height[i] * (j - i)
+                i += 1
+            elif height[i] > height[j]:
+                area = height[j] * (j - i)
+                j -= 1
+            else:
+                area = height[j] * (j - i)
+                i += 1
+                j -= 1
             max_area = max(max_area, area)
         return max_area
 
 
 if __name__ == '__main__':
     print(Solution().maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))
+    print(Solution().maxArea2([1, 8, 6, 2, 5, 4, 8, 3, 7]))
