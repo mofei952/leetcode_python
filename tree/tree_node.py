88d7ee40902dacdc1b7742d71f3e67c05ce8c568
mofei952 <695741200@qq.com>
2021-08-20 23:49:28 +0800

    feat: update TreeNode.create_tree(arr)

--- tree/tree_node.py
+++ tree/tree_node.py
@@ -30,14 +30,14 @@ class TreeNode:
             next_nodes = []
 
         res = []
-        col_count = 2**(level)-1
+        col_count = 2 ** (level) - 1
         for i in range(level):
-            s = (' ' * (2**(level-i)-1)).join(str(node.val) if node else 'N' for node in level_to_nodes_dict[i])
+            s = (' ' * (2 ** (level - i) - 1)).join(str(node.val) if node else 'N' for node in level_to_nodes_dict[i])
             res.append(s.center(col_count))
-            if i != level-1:
-                for j in range(2 ** (level-i-2)):
-                    a = '/' + ' ' * (2*j+1) + '\\'
-                    b = (' ' * (2 ** (level-i)-3-2*j)).join([a] * (2 ** i))
+            if i != level - 1:
+                for j in range(2 ** (level - i - 2)):
+                    a = '/' + ' ' * (2 * j + 1) + '\\'
+                    b = (' ' * (2 ** (level - i) - 3 - 2 * j)).join([a] * (2 ** i))
                     res.append(b.center(col_count))
 
         return '\n'.join(res)
@@ -47,21 +47,47 @@ class TreeNode:
             return False
         return str(self) == str(other)
 
+    # @classmethod
+    # def create_tree(cls, arr):
+    #     if not arr:
+    #         return None
+    #
+    #     val = arr.pop(0)
+    #     if val is None:
+    #         return None
+    #
+    #     node = cls(val)
+    #     node.left = cls.create_tree(arr)
+    #     node.right = cls.create_tree(arr)
+    #
+    #     return node
+
     @staticmethod
-    def create_tree(list):
-        if not list:
+    def create_tree(arr):
+        if not arr:
             return None
-        root = TreeNode(list[0])
+
+        root = TreeNode(arr[0])
         queue = [root]
-        node = None
-        for i, val in enumerate(list[1:]):
-            if i % 2 == 0:
-                node = queue.pop(0)
-                if val is not None:
-                    node.left = TreeNode(val)
-                    queue.append(node.left)
-            else:
-                if val is not None:
-                    node.right = TreeNode(val)
-                    queue.append(node.right)
+
+        for i in range(1, len(arr), 2):
+            node = queue.pop(0)
+
+            val = arr[i]
+            if val is not None:
+                node.left = TreeNode(val)
+                queue.append(node.left)
+
+            if i + 1 >= len(arr):
+                continue
+            val = arr[i + 1]
+            if val is not None:
+                node.right = TreeNode(val)
+                queue.append(node.right)
+
         return root
+
+
+if __name__ == '__main__':
+    tree = TreeNode.create_tree([1, None, 2, 3])
+    print(tree)
