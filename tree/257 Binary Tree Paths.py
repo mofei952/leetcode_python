20ba691bdf5e775e65e56e94de93fcad658701bb
mofei952 <695741200@qq.com>
2021-08-28 18:35:13 +0800

    feat: add solution for the 257th question

--- tree/257 Binary Tree Paths.py
+++ tree/257 Binary Tree Paths.py
@@ -25,8 +25,26 @@ class Solution:
         find(root, str(root.val))
         return res
 
+    def binaryTreePaths2(self, root: Optional[TreeNode]) -> List[str]:
+        if root is None:
+            return ['']
+
+        queue = [(root, str(root.val))]
+        paths = []
+        while queue:
+            node, path = queue.pop(0)
+            if not node.left and not node.right:
+                paths.append(path)
+                continue
+            if node.left:
+                queue.append((node.left, f'{path}->{node.left.val}'))
+            if node.right:
+                queue.append((node.right, f'{path}->{node.right.val}'))
+
+        return paths
+
 
 if __name__ == '__main__':
-    assert Solution().binaryTreePaths(create_tree([1, 2, 3, None, 5])) == ['1->2->5', '1->3']
-    assert Solution().binaryTreePaths(create_tree([1])) == ['1']
-    assert Solution().binaryTreePaths(create_tree([])) == ['']
+    assert set(Solution().binaryTreePaths2(create_tree([1, 2, 3, None, 5]))) == {'1->2->5', '1->3'}
+    assert Solution().binaryTreePaths2(create_tree([1])) == ['1']
+    assert Solution().binaryTreePaths2(create_tree([])) == ['']
