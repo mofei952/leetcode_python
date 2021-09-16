ff01d70e3599f16bfa6584268dfe1ecade3aec18
mofei952 <695741200@qq.com>
2021-09-16 22:15:13 +0800

    perf: optimize solution for the 110th question

--- tree/110 Balanced Binary Tree.py
+++ tree/110 Balanced Binary Tree.py
@@ -1,60 +1,52 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# @Author  : mofei
-# @Time    : 2019/5/18 19:32
-# @File    : 110 Balanced Binary Tree.py
-# @Software: PyCharm
-
 """
-Given a binary tree, determine if it is height-balanced.
-For this problem, a height-balanced binary tree is defined as:
-a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
-
-Example 1:
-Given the following tree [3,9,20,None,None,15,7]:
-
-    3
-   / \
-  9  20
-    /  \
-   15   7
-Return true.
-
-Example 2:
-Given the following tree [1,2,2,3,3,None,None,4,4]:
-
-       1
-      / \
-     2   2
-    / \
-   3   3
-  / \
- 4   4
-Return false.
+question:
+https://leetcode.com/problems/balanced-binary-tree/
 """
 
-from tree.binary_tree import TreeNode, create_tree
+from tree.binary_tree import create_tree
 
 
 class Solution:
-    balance = True
 
     def isBalanced(self, root):
-        def isBalance(node):
-            if not node:
+        def balanced(node):
+            if node is None:
                 return 0
-            h1 = isBalance(node.left)
-            h2 = isBalance(node.right)
+            h1 = balanced(node.left)
+            if h1 == -1:
+                return -1
+            h2 = balanced(node.right)
+            if h2 == -1:
+                return -1
             if abs(h1 - h2) > 1:
-                self.balance = False
+                return -1
             return 1 + max(h1, h2)
 
-        isBalance(root)
-        return self.balance
+        return balanced(root) != -1
+
+    def isBalanced2(self, root):
+        """ 更简洁的写法 """
+
+        def balanced(node):
+            if node is None:
+                return 0
+            h1 = balanced(node.left)
+            h2 = balanced(node.right)
+            if abs(h1 - h2) > 1:
+                raise ValueError('not balanced')
+            return 1 + max(h1, h2)
+
+        try:
+            balanced(root)
+        except ValueError:
+            return False
+        else:
+            return True
 
 
 if __name__ == '__main__':
-    print(Solution().isBalanced(create_tree([1, None, 2, None, 3])))
-    print(Solution().isBalanced(create_tree([3, 9, 20, None, None, 15, 7])))
-    print(Solution().isBalanced(create_tree([1, 2, 2, 3, 3, None, None, 4, 4])))
+    # TODO 比较两种解法的执行效率
+    print(Solution().isBalanced2(create_tree([1, None, 2, None, 3])))
+    print(Solution().isBalanced2(create_tree([3, 9, 20, None, None, 15, 7])))
+    print(Solution().isBalanced2(create_tree([1, 2, 2, 3, 3, None, None, 4, 4])))
+    print(Solution().isBalanced2(create_tree([])))
