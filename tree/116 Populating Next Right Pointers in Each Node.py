83332a1d885301121f157828d8751ce29fd2289b
mofei952 <695741200@qq.com>
2021-10-16 21:55:08 +0800

    feat: add solution for the 116th question

--- tree/116 Populating Next Right Pointers in Each Node.py
+++ tree/116 Populating Next Right Pointers in Each Node.py
@@ -5,6 +5,16 @@ https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
 from tree.binary_tree import create_tree, TreeNode
 
 
+def __init__(self, val=0, left=None, right=None, next=None):
+    self.val = val
+    self.left = left
+    self.right = right
+    self.next = next
+
+
+TreeNode.__init__ = __init__
+
+
 class Solution:
     def connect(self, root: TreeNode) -> TreeNode:
         if root is None:
@@ -21,6 +31,22 @@ class Solution:
                 if node.right:
                     new_queue.append(node.right)
             queue = new_queue
+        return root
+
+    def connect2(self, root: TreeNode) -> TreeNode:
+        if root is None:
+            return None
+        leftmost = root
+        while leftmost:
+            node = leftmost
+            if not node.left:
+                break
+            while node:
+                node.left.next = node.right
+                if node.next:
+                    node.right.next = node.next.left
+                node = node.next
+            leftmost = leftmost.left
         return root
 
 
@@ -48,8 +74,8 @@ def tree_to_list(root):
 
 
 if __name__ == '__main__':
-    res = Solution().connect(create_tree([1, 2, 3, 4, 5, 6, 7]))
+    res = Solution().connect2(create_tree([1, 2, 3, 4, 5, 6, 7]))
     assert tree_to_list(res) == [1, '#', 2, 3, '#', 4, 5, 6, 7, '#']
 
-    res = Solution().connect(create_tree([]))
+    res = Solution().connect2(create_tree([]))
     assert tree_to_list(res) == []
