599904a13a94a0a16c3afd8f9724e43870b50036
mofei952 <695741200@qq.com>
2021-12-04 18:42:03 +0800

    feat: add solution for the 99th question

--- tree/099 Recover Binary Search Tree.py
+++ tree/099 Recover Binary Search Tree.py
@@ -36,30 +36,55 @@ class Solution:
             i += 1
             node = node.right
 
-    first_node = None
-    second_node = None
-    prev_node = TreeNode(float('-inf'))
-
     def recoverTree2(self, root: Optional[TreeNode]) -> None:
         """
         Do not return anything, modify root in-place instead.
         """
-        self.traverse(root)
-        self.first_node.val, self.second_node.val = self.second_node.val, self.first_node.val
+        first = None
+        second = None
+        prev = TreeNode(float('-inf'))
 
-    def traverse(self, root):
-        if root is None:
-            return
+        def traversal(curr):
+            nonlocal first, second, prev
 
-        self.traverse(root.left)
+            if curr is None:
+                return
+            traversal(curr.left)
 
-        if self.first_node is None and self.prev_node.val >= root.val:
-            self.first_node = self.prev_node
-        if self.first_node and self.prev_node.val >= root.val:
-            self.second_node = root
-        self.prev_node = root
+            if first is None and prev.val >= curr.val:
+                first = prev
+            if first and prev.val >= curr.val:
+                second = curr
+            prev = curr
 
-        self.traverse(root.right)
+            traversal(curr.right)
+
+        traversal(root)
+        first.val, second.val = second.val, first.val
+
+    def recoverTree3(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        prev, curr = TreeNode(float('-inf')), root
+        first = second = None
+        stack = []
+
+        while stack or curr:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+
+            if prev.val > curr.val:
+                if first is None:
+                    first = prev
+                second = curr
+            prev = curr
+
+            curr = curr.right
+
+        first.val, second.val = second.val, first.val
 
 
 if __name__ == '__main__':
