0f2cf4bd8221a81bbcd334bb23f61d338e9a1bf2
mofei952 <695741200@qq.com>
2022-01-05 23:18:53 +0800

    style: update style for the 406th question

--- greedy/406 Queue Reconstruction by Height.py
+++ greedy/406 Queue Reconstruction by Height.py
@@ -9,13 +9,13 @@ class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
         queue = []
         for h, k in sorted(people, key=lambda p: (-p[0], p[1])):
-            queue.insert(k, [h, k])
+            queue.insert(k, (h, k))
         return queue
 
 
 if __name__ == '__main__':
     assert Solution().reconstructQueue(
         [[7, 0], [4, 4], [7, 1], [5, 0], [6, 1], [5, 2]]
-    ) == [[5, 0], [7, 0], [5, 2], [6, 1], [4, 4], [7, 1]]
+    ) == [(5, 0), (7, 0), (5, 2), (6, 1), (4, 4), (7, 1)]
     assert Solution().reconstructQueue([]) == []
-    assert Solution().reconstructQueue([[7, 0]]) == [[7, 0]]
+    assert Solution().reconstructQueue([[7, 0]]) == [(7, 0)]
