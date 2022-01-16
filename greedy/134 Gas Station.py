c02cbd4acfc56f41b07b5c9335bd70d1b96ff97c
mofei952 <695741200@qq.com>
2022-01-16 22:43:29 +0800

    feat: add solution for the 134th question

--- greedy/134 Gas Station.py
+++ greedy/134 Gas Station.py
@@ -7,20 +7,38 @@ from typing import List
 
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost):
+            return -1
+
         start, end = len(gas) - 1, 0
-        sum = gas[start] - cost[start]
+        s = gas[start] - cost[start]
         while start > end:
-            if sum >= 0:
-                sum += gas[end] - cost[end]
+            if s >= 0:
+                s += gas[end] - cost[end]
                 end += 1
             else:
                 start -= 1
-                sum += gas[start] - cost[start]
-        return start if sum >= 0 else -1
+                s += gas[start] - cost[start]
+
+        return start if s >= 0 else -1
+
+    def canCompleteCircuit2(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost):
+            return -1
+
+        s = 0
+        start = 0
+        for i in range(len(gas)):
+            s += gas[i] - cost[i]
+            if s < 0:
+                start = i + 1
+                s = 0
+
+        return start
 
 
 if __name__ == '__main__':
-    assert Solution().canCompleteCircuit([1, 2, 3, 4, 5], [3, 4, 5, 1, 2]) == 3
-    assert Solution().canCompleteCircuit([2, 3, 4], [3, 4, 3]) == -1
-    assert Solution().canCompleteCircuit([3], [3]) == 0
-    assert Solution().canCompleteCircuit([1], [3]) == -1
+    assert Solution().canCompleteCircuit2([1, 2, 3, 4, 5], [3, 4, 5, 1, 2]) == 3
+    assert Solution().canCompleteCircuit2([2, 3, 4], [3, 4, 3]) == -1
+    assert Solution().canCompleteCircuit2([3], [3]) == 0
+    assert Solution().canCompleteCircuit2([1], [3]) == -1
