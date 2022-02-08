ab2639e9c418391be4406504303111ae29596675
mofei952 <695741200@qq.com>
2022-02-08 14:59:00 +0800

    style: update style for the 55th question

--- greedy/055 Jump Game.py
+++ greedy/055 Jump Game.py
@@ -1,26 +1,6 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# @Author  : mofei
-# @Time    : 2019/6/25 20:45
-# @File    : 055 Jump Game.py
-# @Software: PyCharm
-
 """
-Given an array of non-negative integers, you are initially positioned at the first index of the array.
-Each element in the array represents your maximum jump length at that position.
-Determine if you are able to reach the last index.
-
-Example 1:
-Input: [2,3,1,1,4]
-Output: true
-Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
-
-Example 2:
-Input: [3,2,1,0,4]
-Output: false
-Explanation: You will always arrive at index 3 no matter what. Its maximum
-             jump length is 0, which makes it impossible to reach the last index.
+question:
+https://leetcode.com/problems/jump-game/
 """
 
 from typing import List
