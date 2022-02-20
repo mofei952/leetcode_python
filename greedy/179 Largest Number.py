b016be50d77486092199979a421e7a6276007231
mofei952 <695741200@qq.com>
2022-02-20 21:22:31 +0800

    feat: add solution for the 179th question

--- greedy/179 Largest Number.py
+++ greedy/179 Largest Number.py
@@ -2,26 +2,37 @@
 question:
 https://leetcode.com/problems/largest-number/
 """
+import functools
 from typing import List
 
 
 class Solution:
+    def compare(self, x, y):
+        return x + y < y + x
+
     def largestNumber(self, nums: List[int]) -> str:
+        nums = [str(n) for n in nums]
         for i in range(1, len(nums)):
             temp = nums[i]
             j = i - 1
-            while j >= 0 and self.compare(nums[j], nums[i]):
+            while j >= 0 and self.compare(nums[j], temp):
+                nums[j + 1] = nums[j]
                 j -= 1
-            for k in range(i, j + 1, -1):
-                nums[k] = nums[k - 1]
             nums[j + 1] = temp
-        return str(int(''.join(map(str, nums))))
+        return str(int(''.join(nums)))
 
-    def compare(self, x, y):
-        return str(x) + str(y) < str(y) + str(x)
+    def compare2(self, x, y):
+        if x + y < y + x:
+            return 1
+        return -1
+
+    def largestNumber2(self, nums: List[int]) -> str:
+        nums = [str(n) for n in nums]
+        nums.sort(key=functools.cmp_to_key(self.compare2))
+        return str(int(''.join(nums)))
 
 
 if __name__ == '__main__':
-    assert Solution().largestNumber([10, 2]) == '210'
-    assert Solution().largestNumber([3, 30, 34, 5, 9]) == '9534330'
-    assert Solution().largestNumber([0, 0]) == '0'
+    assert Solution().largestNumber2([10, 2]) == '210'
+    assert Solution().largestNumber2([3, 30, 34, 5, 9]) == '9534330'
+    assert Solution().largestNumber2([0, 0]) == '0'
