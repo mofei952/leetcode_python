7781dc114084575302964888604c77d9efddb9af
mofei952 <695741200@qq.com>
2022-03-09 23:03:11 +0800

    perf: optimize solution for the 330th question

--- greedy/330 Patching Array.py
+++ greedy/330 Patching Array.py
@@ -7,26 +7,25 @@ from typing import List
 
 class Solution:
     def minPatches(self, nums: List[int], n: int) -> int:
-        combs = {0}
-        for num in nums:
-            combs |= {t + num for t in combs if t + num <= n}
-
-        count = 0
-        for i in range(31):
-            num = 2 ** i
-            if len(combs) > n:
-                print(count)
-                return count
-
-            if num in combs:
-                continue
-
-            combs |= {t + num for t in combs if t + num <= n}
-            count += 1
+        # 如果[1,x]已经被覆盖，将一个小于等于x+1的数y放入数组，则[1,x+y]能被完全覆盖
+        #                     将一个大于    x+1的数y放入数组，则[1,x+y]不能被完全覆盖
+        edge = 0
+        i = 0
+        res = 0
+        while edge < n:
+            if i < len(nums) and nums[i] <= edge + 1:
+                edge += nums[i]
+                i += 1
+            else:
+                # print('add: ', edge + 1)
+                edge = edge * 2 + 1
+                res += 1
+        print(res)
+        return res
 
 
 if __name__ == '__main__':
-    Solution().minPatches([1, 3], 6)
-    Solution().minPatches([1, 5, 10], 20)
-    Solution().minPatches([1, 2, 2], 5)
-    Solution().minPatches([1, 2, 31, 33], 2147483647)
+    assert Solution().minPatches([1, 3], 6) == 1
+    assert Solution().minPatches([1, 5, 10], 20) == 2
+    assert Solution().minPatches([1, 2, 2], 5) == 0
+    assert Solution().minPatches([1, 2, 31, 33], 2147483647) == 28
