575ae3bbf929c1cdfc86e1d4758cd347332d7ebe
mofei952 <695741200@qq.com>
2022-04-05 12:02:32 +0800

    feat: add solution for the 135th question

--- greedy/135 Candy.py
+++ greedy/135 Candy.py
@@ -17,11 +17,36 @@ class Solution:
         print(candies, sum(candies))
         return sum(candies)
 
+    def candy2(self, ratings: List[int]) -> int:
+        up = 1
+        down = 0
+        peak = 1
+        res = 1
+        for i in range(1, len(ratings)):
+            if ratings[i] > ratings[i - 1]:
+                up += 1
+                down = 0
+                res += up
+                peak = up
+            elif ratings[i] < ratings[i - 1]:
+                down += 1
+                up = 1
+                res += down
+                if down >= peak:
+                    res += 1
+            else:
+                up = 1
+                down = 0
+                res += up
+                peak = up
+        return res
+
 
 if __name__ == '__main__':
-    assert Solution().candy([1, 0, 2]) == 5
-    assert Solution().candy([1, 2, 2]) == 4
-    assert Solution().candy([1, 2, 3, 2]) == 7
-    assert Solution().candy([2, 3, 2, 1]) == 7
-    assert Solution().candy([2, 1, 0, 1]) == 8
-    assert Solution().candy([2, 0, 1, 2]) == 8
+    assert Solution().candy2([1, 0, 2]) == 5
+    assert Solution().candy2([1, 2, 2]) == 4
+    assert Solution().candy2([1, 2, 3, 2]) == 7
+    assert Solution().candy2([2, 3, 2, 1]) == 7
+    assert Solution().candy2([2, 1, 0, 1]) == 8
+    assert Solution().candy2([2, 0, 1, 2]) == 8
+    assert Solution().candy2([1, 3, 2, 2, 1]) == 7
