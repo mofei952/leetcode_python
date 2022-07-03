6584fe56b2fdfce94d9e3f004c810681b5b48c2f
mofei952 <695741200@qq.com>
2022-07-03 14:53:22 +0800

    perf: optimize solution for the 05.08 question

--- array/05.08 draw-line-lcci.py
+++ array/05.08 draw-line-lcci.py
@@ -2,47 +2,36 @@
 https://leetcode.cn/problems/draw-line-lcci/
 """
 from typing import List
+import ctypes
 
 
 class Solution:
     def drawLine(self, length: int, w: int, x1: int, x2: int, y: int) -> List[int]:
-        row_nums = w // 32
-        index1 = x1 // 32 + y * row_nums
-        index2 = x2 // 32 + y * row_nums
+        cols = w // 32
+        start = y * cols + x1 // 32
+        end = y * cols + x2 // 32
+        res = [0] * length
 
-        res = [0 for _ in range(index1)]
+        for i in range(start + 1, end):
+            res[i] = -1
 
-        for i in range(index1, index2 + 1):
-            if index1 < i < index2:
-                res.append(-1)
-                continue
+        res[start] = 0xffffffff >> x1 % 32
+        temp_end = (0xffffffff << (31 - x2 % 32)) & 0xffffffff
+        res[end] = temp_end & res[start] if start == end else temp_end
 
-            negative = False
-            bit_index1 = x1 % 32 if i == index1 else 0
-            if bit_index1 == 0:
-                negative = True
-                bit_index1 = 1
-            num = 2 ** (32 - bit_index1) - 1
-
-            if i == index2:
-                bit_index2 = x2 % 32
-                num -= 2 ** (32 - bit_index2 - 1) - 1
-
-            if negative:
-                num = -(2 ** 31 - num)
-
-            res.append(num)
-
-        for _ in range(index2 + 1, length):
-            res.append(0)
+        for i in [start, end]:
+            if res[i] & 0x80000000 == 0x80000000:
+                res[i] = -((res[i] ^ 0xffffffff) + 1)
 
         print(res)
         return res
 
 
 if __name__ == '__main__':
-    Solution().drawLine(1, 32, 30, 31, 0)
-    Solution().drawLine(3, 96, 0, 95, 0)
-    Solution().drawLine(15, 96, 81, 95, 1)
-    Solution().drawLine(24, 96, 2, 19, 5)
-    Solution().drawLine(2, 64, 0, 52, 0)
+    assert Solution().drawLine(1, 32, 30, 31, 0) == [3]
+    assert Solution().drawLine(3, 96, 0, 95, 0) == [-1, -1, -1]
+    assert Solution().drawLine(15, 96, 81, 95, 1) == [0, 0, 0, 0, 0, 32767, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    assert Solution().drawLine(24, 96, 2, 19, 5) == [
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 1073737728, 0, 0, 0, 0, 0, 0, 0, 0]
+    assert Solution().drawLine(2, 64, 0, 52, 0) == [-1, -2048]
